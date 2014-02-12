92ad7b2b30df23587f6f71f486e8a711a76d31e7
Sergey Chernobrivenko <ser4@me.com>
2014-02-12 11:25:13 +0400

    pointcut

--- SpringAOP/src/main/java/ru/ser4/aspect/LoggingAspect.java
+++ SpringAOP/src/main/java/ru/ser4/aspect/LoggingAspect.java
@@ -2,12 +2,25 @@ package ru.ser4.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-	@Before("execution(public * get*())")
+	@Pointcut("execution(* get*(..))")
+	public void allGetters() {}
+	
+	@Pointcut("within(ru.ser4.model.Circle)")
+	public void circleMethods() {}
+
+	@Before("allGetters() && circleMethods()")
 	public void loggingAdvice()	{
 		System.out.println("Advice run");
+	}
+	
+	@Before("circleMethods()")
+	public void secondAdvice()
+	{
+		System.out.println("Another advice run");
 	}
 }
